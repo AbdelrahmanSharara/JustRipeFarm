a766b30785204cec65f12997bb08b074ce2d42f9
Reece Waller <RW613@student.anglia.ac.uk>
2018-11-26 22:23:13 +0000

    Working datagrid on Storage section!

--- JustRIpe/Events/DashboardEvent.xaml.cs
+++ JustRIpe/Events/DashboardEvent.xaml.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace JustRIpe
 {
@@ -25,7 +28,13 @@ namespace JustRIpe
             InitializeComponent();
         }
 
+        //crop button
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+        private void Close()
         {
 
         }
--- JustRIpe/Events/StorageEvent.xaml.cs
+++ JustRIpe/Events/StorageEvent.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
 
 namespace JustRIpe
 {
@@ -20,9 +23,41 @@ namespace JustRIpe
     /// </summary>
     public partial class StorageEvent : UserControl
     {
+        string connectionString = "";
         public StorageEvent()
         {
             InitializeComponent();
+        }
+
+        private void Load_table_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter("", sqlCon);
+                DataSet dtg1 = new DataSet();
+                sqlData.Fill(dtg1);
+
+            }
+
+
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Do not load your data at design time.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Load your data here and assign the result to the CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
         }
     }
 }
