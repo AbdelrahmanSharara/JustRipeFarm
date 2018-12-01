7c4cc2567fcbb020e030f698eb5f8453f17588cb
Reece W <rw613@student.anglia.ac.uk>
2018-12-01 22:58:45 +0000

    Finished timetable event, sql queries done

--- JustRIpe/Events/TimetableEvent.xaml.cs
+++ JustRIpe/Events/TimetableEvent.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace JustRIpe
 {
@@ -23,6 +26,41 @@ namespace JustRIpe
         public TimetableEvent()
         {
             InitializeComponent();
+            FillEventDataGrid();
+        }
+
+        string connectionString = Properties.Settings.Default.DBAccess;
+
+        private void FillEventDataGrid()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+                sqlCon.Open(); //establishing sql connection
+                SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM Events", sqlCon);
+                    //("SELECT eventID, `Crops`.`cropName`, `Vehicles`.`vehicleName`, eventName, eventDescription, eventDate, eventTime FROM `Events` LEFT JOIN `Crops` ON `Events`.`cropID` = `Crops`.`cropsID`", sqlCon); //sql statement for event main
+                DataTable TimetableDSet = new DataTable(); //defining storage dataset
+
+                sqlData.Fill(TimetableDSet); //filling timetable database
+
+                eventsDataGrid.ItemsSource = TimetableDSet.DefaultView;
+            }
+
+        }
+
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Do not load your data at design time.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Load your data here and assign the result to the CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
         }
     }
 }
+
+
